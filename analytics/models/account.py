6f65a97fc15aebd86d3a97df5f48e960999f4f4e
yeongher <wangyh.opengoss@gmail.com>
2022-06-16 16:13:04 +0800

    Init project

--- analytics/models/account.py
+++ analytics/models/account.py
@@ -1,5 +1,9 @@
+from .base_model import ArrangoDBModel
 
-class Account:
+
+class Account(ArrangoDBModel):
+
+    __NAME__ = "Accounts"
 
     def __init__(self):
         pass
--- analytics/server.py
+++ analytics/server.py
@@ -1,11 +1,27 @@
 import logging
 
 from fastapi import FastAPI
+from starlette import status
 
-from analytics.models.base_model import initialize_database
+from .models.account import Account
+from .models.base_model import initialize_database
 
+COLLECTIONS_NAME = [Account.__NAME__]
+
+
+class StaticFileAccessLogFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        http_status = record.args[-1]
+        uri = record.args[-3]
+        if int(http_status) == status.HTTP_200_OK:
+            return not uri.endswith('.html') and not uri.endswith('.css') \
+                   and not uri.endswith('.js') and not uri.endswith('.js.map') and not uri.endswith('ico')
+        return False
+
+
+logging.getLogger("uvicorn.access").addFilter(StaticFileAccessLogFilter())
 logging.basicConfig(filename='logging.conf')
 
 app = FastAPI()
 
-initialize_database()
+initialize_database(COLLECTIONS_NAME)
