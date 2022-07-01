21a8bc29ac562806b8ab144d6a9d84187a5a9fb8
yeongher <wangyh.opengoss@gmail.com>
2022-07-01 11:12:02 +0800

    Init project

--- ui/src/components/Menu.vue
+++ ui/src/components/Menu.vue
@@ -1,12 +1,8 @@
 <template>
-  <li class="nav-item active">
+  <li class="nav-item">
     <a class="nav-link" :href="url">
           <span class="nav-link-icon d-md-none d-lg-inline-block">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                 stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                 stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline
-                points="5 12 3 12 12 3 21 12 19 12"/><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"/><path
-                d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"/></svg>
+            <svg-icon class-name="icon icon-tabler icon-tabler-layout-dashboard" :name="icon"/>
           </span>
       <span class="nav-link-title">{{ name }}</span>
     </a>
@@ -14,6 +10,8 @@
 </template>
 
 <script>
+import SvgIcon from '@/components/SvgIcon'
+
 export default {
   // eslint-disable-next-line
   name: "Menu",
@@ -30,10 +28,9 @@ export default {
       type: String,
       required: true
     }
+  },
+  components: {
+    SvgIcon
   }
 }
 </script>
-
-<style scoped>
-
-</style>
