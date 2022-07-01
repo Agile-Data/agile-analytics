a7263971e9840faa2255abbc00d785ff96ee06ae
yeongher <wangyh.opengoss@gmail.com>
2022-07-01 10:59:31 +0800

    Init project

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -34,16 +34,15 @@ module.exports = defineConfig({
         }
     },
     chainWebpack: config => {
-    //svg 配置
-    config.module.rules.delete("svg"); //重点：删除默认配置中处理 svg
-    config.module.rule('svg-sprite-loader').test(/\.svg$/)
-      .include
-      .add(path.resolve('./src/assets/svg')) //处理 svg 保存路径
-      .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]'  //给 symbo 配置 id
-      })
-  },
+        config.module.rules.delete("svg");
+        config.module.rule('svg-sprite-loader').test(/\.svg$/)
+            .include
+            .add(path.resolve('./src/assets/svg'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
+    },
 })
